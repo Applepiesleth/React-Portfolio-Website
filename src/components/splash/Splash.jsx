7b7ac8fa003fda8e4270276b0e1e02d7e1b4cca8
Ronan Sandoval <ronanchristophersandoval1@gmail.com>
2022-02-23 02:56:46 -0700

    splash page

--- src/components/splash/Splash.jsx
+++ src/components/splash/Splash.jsx
@@ -3,7 +3,10 @@ import "./splash.scss"
 export default function Splash() {
   return (
     <div className="splash" id="splash">
-
+        <div className="box">
+          <h1>Ronan Sandoval</h1>
+          <h2>Software and Game Dev / Digital Artist</h2>
+        </div>
     </div>
   )
 }
