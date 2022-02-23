74632cf4461851854268cb0f9339d8ff4a431618
Ronan Sandoval <ronanchristophersandoval1@gmail.com>
2022-02-22 18:19:14 -0700

    navbar

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,10 +5,15 @@ export default function Topbar() {
     <div className="topbar">
         <div className="wrapper">
             <div className="left">
-                <a href="#intro">asdas</a>
+                <a href="#intro" className="logo">Ronan Sandoval</a>
+                <div className="linksContainer">
+                    <a href="#intro" className="link">About Me</a>
+                    <a href="#portfolio" className="link">Portfolio</a>
+                    <a href="#works" className="link">Works</a>
+                    <a href="#contact" className="link">Contact</a>
+                </div>
             </div>
             <div className="right">
-
             </div>
         </div>  
     </div>
